8eb4f9e00c033af4e618934957a503f7b6193ef0
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2026-05-19 02:59:24 +0500

    update

--- src/components/AccountSettingsForm.tsx
+++ src/components/AccountSettingsForm.tsx
@@ -55,6 +55,7 @@ export function AccountSettingsForm({ user }: AccountSettingsFormProps) {
 
   async function handleEmail(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    
     const formData = new FormData(event.currentTarget);
 
     try {
